fc51348395c093dec379fe78951df435c30a7e27
Ivan <ivan@parentmap.hk>
2026-02-14 09:28:34 +0800

    Add sitemap generation with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,28 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://parentmap.hk',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  outDir: 'dist',
+  additionalPaths: async (config) => {
+    const locationsData = require('./src/data/locations.json');
+    
+    const generateSlug = (name) => {
+      return name
+        .toLowerCase()
+        .replace(/[^\w\s\u4e00-\u9fa5-]/g, '')
+        .replace(/\s+/g, '-')
+        .replace(/-+$/, '')
+        .substring(0, 50);
+    };
+    
+    const placeUrls = locationsData.locations.map((place) => ({
+      loc: `/place/${generateSlug(place.name)}`,
+      lastmod: place.checkedAt || new Date().toISOString(),
+      changefreq: 'weekly',
+      priority: 0.8,
+    }));
+    
+    return placeUrls;
+  },
+};
